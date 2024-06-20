fffdfdc4a2149038109b93c060771ecfd8084fec
greenmelon <suren78621@gmail.com>
2024-06-20 12:21:05 +0545

    auth signup hash

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const hashPassword = await bcryptjs.hash(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashPassword,
+  });
+
+  try {
+    await newUser.save();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+  res.status(201).json({ message: "Signup successful" });
+};
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -15,10 +16,13 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/test", (req, res) => {
   res.json({ message: "API is working!" });
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
