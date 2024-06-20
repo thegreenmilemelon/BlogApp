e04956e46a7126da0f00a7613a6d185d0b2b50cd
greenmelon <suren78621@gmail.com>
2024-06-20 18:17:17 +0545

    update header component with user data

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,13 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
-
+import { useSelector } from "react-redux";
 import { useState } from "react";
 
 const Header = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div>
       <Navbar className="border-b-2">
@@ -38,11 +39,33 @@ const Header = () => {
           <Button className="w-12 h-10 hidden sm:inline" color="gray">
             <FaMoon />
           </Button>
-          <Link to="/sign-in">
-            <Button gradientDuoTone="purpleToBlue" outline>
-              Sign In
-            </Button>
-          </Link>
+          {currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar alt="user" img={currentUser.profilePicture} rounded />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">@{currentUser.username}</span>
+                <span className="block text-sm font-medium truncate">
+                  {currentUser.email}
+                </span>
+              </Dropdown.Header>
+              <Link to={"/dashboard?tab=profile"}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign out</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link to="/sign-in">
+              <Button gradientDuoTone="purpleToBlue" outline>
+                Sign In
+              </Button>
+            </Link>
+          )}
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
