1adf6f88e94638628db1c87cea56217a40244281
greenmelon <suren78621@gmail.com>
2024-06-20 11:38:39 +0545

    create test user api route

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,24 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
+import userRouter from "./routes/user.route.js";
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+
+app.get("/test", (req, res) => {
+  res.json({ message: "API is working!" });
+});
+
+app.use("/api/users", userRouter);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
