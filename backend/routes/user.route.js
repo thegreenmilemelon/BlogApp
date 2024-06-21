383d4ca1e299bac9d210a67cff4758d8de8a9a5a
greenmelon <suren78621@gmail.com>
2024-06-21 13:16:26 +0545

    add update user api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { test, signout } from "../controllers/user.controller.js";
+import { test, updateUser, signout } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.post("/signout", signout);
+router.put("/update/:userId", verifyToken, updateUser);
 
 export default router;
